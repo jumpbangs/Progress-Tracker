f3011013292eae1b9d843a97cbb536185b25ff87
Amit <jumpbang.dev@gmail.com>
2020-01-20 17:52:09 +0545

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './Style/App.css';
+import './Style/Style.css';
+import './Style/Reset.css';
+
+// Bootstrap
+import './Style/bootstrap.min.css';
+
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+
+
+class App extends Component{
+	render() {
+		return(
+			// <Login />
+			<Register />
+		)}
 }
 
 export default App;
